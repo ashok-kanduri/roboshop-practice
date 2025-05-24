2baf223bda086cce83a182b99ae87ec2e5bdfda2
ashokkumar <ashkanduri@gmail.com>
2025-05-24 22:04:21 +0530

    make the changes

---
+++ redis.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "script started executing at: $(date)" 
+
+if [ $USERID -eq 0 ]
+    then
+        echo "your are running with root access" 
+    else 
+        echo -e "$R ERROR:: please run this script using root access $N"
+        exit 1
+    fi
+
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is.... $G SUCCESSFUL $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is.... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling default redis"
+
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "enabling redis:7"
+
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Installing redis"
+
+systemctl enable redis &>>$LOG_FILE
+systemctl start redis
+VALIDATE $? "Starting redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c proctected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
+VALIDATE $? "enabling remote connections"
+
+systemctl restart redis
+VALIDATE $? "Restarting redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=(( $END_TIME - $START_TIME ))
+
+echo -e "script execution completed succesfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
