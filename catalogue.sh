fd851c516571ffb317018efaf8d9ecd83662f815
ashokkumar <ashkanduri@gmail.com>
2025-05-24 18:39:07 +0530

    make the changes

---
+++ catalogue.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
+USER_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "script started executing at: $(date)" 
+
+if [ $USERID -eq 0 ]
+    then
+        echo "your are running with root access" 
+    else 
+        echo -e "$R ERROR:: please run this script using root access $N"
+        exit 1
+    fi
+
+ cp $USER_DIR/mongodb.repo /etc/yum.repos.d/mongodb.repo 
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is.... $G SUCCESSFUL $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is.... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling nodejs:20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing nodejs:20"
+
+useradd --system --home /app --shell /sbin/nologin --comment "system user roboshop" roboshop $LOG_FILE
+VALIDATE $? "creating system user roboshop"
