5e0ceab0dc33c6a66d69accaf1f81bb528535800
ashokkumar <ashkanduri@gmail.com>
2025-05-25 01:12:20 +0530

    make the changes

--- redis.sh
+++ redis.sh
@@ -9,27 +9,28 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
-echo "script started executing at: $(date)" 
+echo "script started executing at: $(date)" | tee -a $LOG_FILE
 
-if [ $USERID -eq 0 ]
-    then
-        echo "your are running with root access" 
-    else 
-        echo -e "$R ERROR:: please run this script using root access $N"
-        exit 1
-    fi
+#check if the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R ERROR:: please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1
+else 
+    echo "you are running with root access" | tee -a $LOG_FILE
+fi
 
-
+#validate functions take input as exit status for the command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is.... $G SUCCESSFUL $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is.... $R FAILURE $N" | tee -a $LOG_FILE
+    then 
+        echo -e "$2 is .... $G SUCCESSFUL $N" | tee -a $LOG_FILE
+    else 
+        echo -e "$2 is .... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -47,9 +48,7 @@ sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c proctected-mode no' /et
 VALIDATE $? "enabling remote connections"
 
 systemctl enable redis &>>$LOG_FILE
-VALIDATE $? "enabling redis"
-
-systemctl start redis &>>$LOG_FILE
+systemctl start redis
 VALIDATE $? "Starting redis"
 
 END_TIME=$(date +%s)
