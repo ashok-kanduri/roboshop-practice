b7cdfa8ca4e9eb5a667442bf3dfd5791153fe0f3
ashokkumar <ashkanduri@gmail.com>
2025-05-25 12:26:37 +0530

    make the changes

--- rediscopy.sh
+++ rediscopy.sh
@@ -39,19 +39,17 @@ dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling default redis"
 
 dnf module enable redis:7 -y &>>$LOG_FILE
-VALIDATE $? "Enabling redis:7"
+VALIDATE $? "enabling redis:7"
 
 dnf install redis -y &>>$LOG_FILE
-VALIDATE $? "installing redis:7"
+VALIDATE $? "Installing redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "editing redis.conf to accept remote connections"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c proctected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
+VALIDATE $? "enabling remote connections"
 
 systemctl enable redis &>>$LOG_FILE
-VALIDATE $? "enabling redis"
-
-systemctl start redis &>>$LOG_FILE
-VALIDATE $? "starting redis"
+systemctl start redis
+VALIDATE $? "Starting redis"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=(( $END_TIME - $START_TIME ))
